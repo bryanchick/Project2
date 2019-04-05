9777210a10482557b478ffc89cf9f3fd7a65032e
Bryan <dbryanchick@gmail.com>
2019-04-04 21:09:30 -0600

    updating app.py, index,

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/Beatport.sqlite"
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
@@ -26,10 +26,9 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(db.engine, reflect=True)
 
+songs_table = Base.classes.tab
 # Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
-
+# Samples_Metadata = Base.classes.tab
 
 @app.route("/")
 def index():
@@ -37,67 +36,72 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/names")
-def names():
+@app.route("/songs")
+def songs():
     """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
+    unique_songs = db.engine.execute("select distinct(track_title) FROM tab").fetchall()
+    print(unique_songs, "is my unique songs")
+    unique_songs = [ song[0] for song in unique_songs]
+    return jsonify(unique_songs)
 
 
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
+@app.route('/songs/<song_name>')
+def get_song_stats(songs_table):
+    pass
+    # go to the database, select the cols you want
+    # where the track_title = song_name
+    # return a JSON dataset that plotlyJS can consume for the chart
+    # this route, will be called when the dropdown value changes
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+# @app.route("/metadata/<sample>")
+# def sample_metadata(sample):
+#     """Return the MetaData for a given sample."""
+#     sel = [
+#         Samples_Metadata.sample,
+#         Samples_Metadata.ETHNICITY,
+#         Samples_Metadata.GENDER,
+#         Samples_Metadata.AGE,
+#         Samples_Metadata.LOCATION,
+#         Samples_Metadata.BBTYPE,
+#         Samples_Metadata.WFREQ,
+#     ]
 
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
+#     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
 
-    print(sample_metadata)
-    return jsonify(sample_metadata)
+    # # Create a dictionary entry for each row of metadata information
+    # sample_metadata = {}
+    # for result in results:
+    #     sample_metadata["sample"] = result[0]
+    #     sample_metadata["ETHNICITY"] = result[1]
+    #     sample_metadata["GENDER"] = result[2]
+    #     sample_metadata["AGE"] = result[3]
+    #     sample_metadata["LOCATION"] = result[4]
+    #     sample_metadata["BBTYPE"] = result[5]
+    #     sample_metadata["WFREQ"] = result[6]
+
+    # print(sample_metadata)
+    # return jsonify(sample_metadata)
 
 
-@app.route("/samples/<sample>")
-# probally going to bu used for piechart data
-# the <sample> here is the same ID as the <sample> at the medadata
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+# @app.route("/samples/<sample>")
+# # probally going to bu used for piechart data
+# # the <sample> here is the same ID as the <sample> at the medadata
+# def samples(sample):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(tab).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[sample].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,103 +1,104 @@
 function buildMetadata(sample) {
 
-    d3.json(`metadata/${sample}`).then(function (data) {
-      console.log(data)
-      d3.select('#sample-metadata').html('')
-      let myHtmlblock = d3.select('#sample-metadata');
-      Object.keys(data).forEach(key => {
-        myHtmlblock.append('p').text(key + " : " + data[key])
-      })
-  
+  d3.json(`metadata/${sample }`).then(function (data) {
+    console.log(data)
+    d3.select('#sample-metadata').html('')
+    let myHtmlblock = d3.select('#sample-metadata');
+    Object.keys(data).forEach(key => {
+      myHtmlblock.append('p').text(key + " : " + data[key])
     })
-  }
-  
-  // Use sample_values as the values for the PIE chart
-  // Use otu_ids as the labels for the pie chart
-  // Use otu_labels as the hovertext for the chart
-  
-  function buildCharts(sample) {
-    console.log(sample)
-    let url = `samples/${sample}`;
-    d3.json(url).then(function (data) {
-  
-      let myvalues = data['Date Pulled'];
-      let myLables = data['Chart Rank'];
-      let myvalues1 = data.sample_values;
-      let myLables1 = data.otu_ids;
-      // Plotly Pie Chart
-      var trace1 = {
-        x: myvalues,
-        y: myLables,
-        type: 'scatter'
-      };
-      
-      var data = [trace1];
-      
-      Plotly.newPlot('myDiv', data);
-      var staticData = [{
-        values: myvalues,
-        labels: myLables,
-        type: 'scatter'
-      }];
-  
-      var layout = {
-        height: 400,
-        width: 500
-      };
-  
-      Plotly.newPlot('pie', staticData, layout);
-      // start bubble chart from plotly
-      var trace1 = {
-        y: myvalues1,
-        x: myLables1,
-        mode: 'markers',
-        marker: {
-          color: myLables1,
-          opacity: myvalues1,
-          size: myvalues1,
-        }
-      };
-  
-      var data = [trace1];
-  
-      var layout = {
-        title: 'otu_lables',
-        showlegend: true,
-        height: 600,
-        width: 1000
-      };
-  
-      Plotly.newPlot('bubble', data, layout);
-    })
-  }
-  
-  
-  function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-  
-    // Use the list of sample names to populate the select options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  
-      // Use the first sample from the list to build the initial plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
+
+  })
+}
+
+// Use sample_values as the values for the PIE chart
+// Use otu_ids as the labels for the pie chart
+// Use otu_labels as the hovertext for the chart
+
+function buildCharts(song_name) {
+  console.log(song_name)
+  let url = `/songs/${song_name}`
+
+  d3.json(url).then(function (data) {
+
+    console.log(data)
+
+    let myvalues = data['Date Pulled'];
+    let myLables = data['Chart Rank'];
+    // Plotly scatter Chart
+    var trace1 = {
+      x: myvalues,
+      y: myLables,
+      type: 'scatter'
+    };
+    
+    var data = [trace1];
+    
+    Plotly.newPlot('scatter', data);
+    var staticData = [{
+      values: myvalues,
+      labels: myLables,
+      type: 'scatter'
+    }];
+
+    var layout = {
+      height: 400,
+      width: 500
+    };
+
+    Plotly.newPlot('scatter', staticData, layout);
+    // // start bubble chart from plotly
+    // var trace1 = {
+    //   y: myvalues1,
+    //   x: myLables1,
+    //   mode: 'markers',
+    //   marker: {
+    //     color: myLables1,
+    //     opacity: myvalues1,
+    //     size: myvalues1,
+    //   }
+    // };
+
+    // var data = [trace1];
+
+    // var layout = {
+    //   title: 'otu_lables',
+    //   showlegend: true,
+    //   height: 600,
+    //   width: 1000
+    // };
+
+    // Plotly.newPlot('bubble', data, layout);
+  })
+}
+
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/songs").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
     });
-  }
-  
-  function optionChanged(newSample) {
-    // recieves the ID of the person who gave the sample
-    // Fetch new data each time a new sample is selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
-  }
-  
-  // Initialize the dashboard
-  init();
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // recieves the ID of the person who gave the sample
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
